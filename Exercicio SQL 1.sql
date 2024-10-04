ebd22516f46010d2b5d1399d935140f5f9aff90c
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2024-10-04 16:14:20 -0300

    .

---
+++ Exercicio SQL 1.sql
@@ -0,0 +1,37 @@
+USE ContosoRetailDW
+
+--DESAFIO 1-A
+--SELECT ProductKey
+--FROM DimProduct
+--O numero de produtos continua igual
+
+-- DESAFIO 1-B
+--SELECT CustomerKey
+--FROM DimCustomer
+--O numero de clientes diminuiu
+
+--DESAFIO 2
+--SELECT 
+--CustomerKey AS Cliente,
+--FirstName AS Nome,
+--EmailAddress AS Email,
+--BirthDate AS 'Data de Nascimento'
+--FROM dimCustomer
+
+--DESAFIO 3-A
+--SELECT TOP(100)
+--FirstName AS Nome, 
+--EmailAddress AS Sobrenome,
+--BirthDate AS 'Data de nascimento'
+--FROM DimCustomer
+
+--DESAFIO 3-B
+--SELECT TOP(20) PERCENT FirstName, LastName FROM DimCustomer
+
+--DESAFIO 4
+--SELECT BrandName AS Fornecedor FROM DimProduct
+
+--DESAFIO 5
+-- SELECT DISTINCT ProductKey FROM DimProduct
+-- SELECT DISTINCT ProductKey FROM FactSales
+--Um produto ficou sem ser vendido
